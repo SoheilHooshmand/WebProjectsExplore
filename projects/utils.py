e0b7ff681ad15d19a032e14573578ab412e0c73b
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-05-21 16:58:41 +0330

    create utils.py

---
+++ projects/utils.py
@@ -0,0 +1,37 @@
+from .models import Project, Tag
+from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+def searchProject(request):
+    search_query = ""
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+    tags = Tag.objects.filter(name__icontains=search_query)
+    projects = Project.objects.distinct().filter(Q(name__icontains=search_query)|
+                                                 Q(descriptions__icontains=search_query)|
+                                                 Q(owner__name__icontains=search_query)|
+                                                 Q(tags__in=tags))
+    return projects, search_query
+
+
+def paginateProject(request, projects, results):
+    page = request.GET.get('page')
+    paginator = Paginator(projects, results)
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+    leftIndex = int(page) - 4
+    # leftIndex = int(page) - 1
+    if leftIndex < 1:
+        leftIndex = 1
+    rightIndex = int(page) + 5
+    # rightIndex = int(page) + 2
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+
+    custom_range = range(leftIndex, rightIndex)
+    return custom_range, projects
