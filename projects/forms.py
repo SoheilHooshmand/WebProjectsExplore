a8cea9914367f571fd5ee640a7edb97afc2d742e
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-05-21 16:56:49 +0330

    create form.py

---
+++ projects/forms.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm
+from .models import Project, Review
+from django import forms
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name','image','descriptions', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, *args, **kwargs):
+         super(ProjectForm, self).__init__(*args, **kwargs)
+         for name, field in self.fields.items():
+             field.widget.attrs.update({'class' : 'input', 'placeholder' : 'Add '+name})
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+        labels = {
+            'value':'Place your vote',
+            'body':'Add a comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input', 'placeholder': 'Add ' + name})
