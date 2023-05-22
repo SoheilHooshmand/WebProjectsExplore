95da91c8522aa6b2a4af9e7b228ad986720e5edc
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-05-22 15:31:48 +0330

    create admin.py

---
+++ users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile, Skil, Message
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Skil)
+admin.site.register(Message)
+
