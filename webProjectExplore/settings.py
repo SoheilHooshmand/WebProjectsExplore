51d6c5ca6594ee616c084b4b076b81f9e6e9f9be
SoheilHooshmand <99813498+SoheilHooshmand@users.noreply.github.com>
2023-07-20 11:47:18 +0330

    Update settings.py

--- webProjectExplore/settings.py
+++ webProjectExplore/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'web_project_explore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'webprojectexplore',
+        'USER' : 'root',
+        'PASSWORD' : 'Sf$77777',
+        'PORT' : '3306',
+        'OPTIONS' : {
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
